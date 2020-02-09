6e68bc9ea85919b9de1e70acd60b6450b03c9368
Molly Murtha <mollymurtha@mollys-mbp-2.home>
2020-02-08 22:07:13 -0500

    update

--- lab4/index.js
+++ lab4/index.js
@@ -5,13 +5,47 @@ const port = 80;
 var chalk = require('chalk');
 console.log(chalk.red("Testing!"));
 
+// const Pokemon = require(json-pokemon);
+// let pokemon = new Pokemon();
+// not sure how to return the json objects from json-pokemon
+
 App.get("/id/:id", (req, res) => {
-    res.send('Pokemon' + req.params.id);
+    let result = [];
+
+        this.pokemon.forEach((value)=> {
+            if(value.color == req.params.color) {
+                result.push(value);
+            }
+        });
+
+        if(result.length == 0) {
+            //Invalid
+            result.push({"error": "Can't find!"});
+        } else {
+            //Valid
+        }
+
+        return result;
 });
 
 
 App.get("/name/:name", (req, res) => {
-    res.send(people.readName(req));
+    let result = [];
+
+        this.pokemon.forEach((value)=> {
+            if(value.color == req.params.color) {
+                result.push(value);
+            }
+        });
+
+        if(result.length == 0) {
+            //Invalid
+            result.push({"error": "Can't find!"});
+        } else {
+            //Valid
+        }
+
+        return result;
 });
 
 
