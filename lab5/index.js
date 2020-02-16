3c0b781b80e75746819a0d8d589581e4f4a143ec
Molly Murtha <mollymurtha@Mollys-MacBook-Pro-2.local>
2020-02-15 23:35:53 -0500

    lab5

---
+++ lab5/index.js
@@ -0,0 +1,61 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+var chalk = require('chalk');
+console.log(chalk.red("Testing!"));
+
+// const Pokemon = require(json-pokemon);
+// let pokemon = new Pokemon();
+// not sure how to return the json objects from json-pokemon
+
+// add express middleware
+App.use(Express.json());
+// add static route
+App.use("/", Express.static("public"));
+
+
+App.get("/id/:id", (req, res) => {
+    
+    let result = [];
+
+        this.pokemon.forEach((value)=> {
+            if(value.color == req.params.color) {
+                result.push(value);
+            }
+        });
+
+        if(result.length == 0) {
+            //Invalid
+            result.push({"error": "Can't find!"});
+        } else {
+            //Valid
+        }
+
+        return result;
+});
+
+
+App.get("/name/:name", (req, res) => {
+    let result = [];
+
+        this.pokemon.forEach((value)=> {
+            if(value.color == req.params.color) {
+                result.push(value);
+            }
+        });
+
+        if(result.length == 0) {
+            //Invalid
+            result.push({"error": "Can't find!"});
+        } else {
+            //Valid
+        }
+
+        return result;
+});
+
+
+App.listen(port, function() {
+    console.log("Sever running!");
+});
