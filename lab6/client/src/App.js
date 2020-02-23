7ad9baa626701a80b82141106209bd0e76c32774
Molly Murtha <mollymurtha@Mollys-MacBook-Pro-2.local>
2020-02-22 22:56:02 -0500

    update

--- lab6/client/src/App.js
+++ lab6/client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IdSearch from './components/IdSearch/index.js';
 import NameSearch from './components/NameSearch/index.js';
