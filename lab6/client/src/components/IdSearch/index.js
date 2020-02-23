83a9e86fba02b49095f7eb5778297f1b5fd46a14
Molly Murtha <mollymurtha@Mollys-MacBook-Pro-2.local>
2020-02-22 22:52:02 -0500

    update

---
+++ lab6/client/src/components/IdSearch/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class IdSearch extends React.Component {
+    readId(event) {
+
+        event.preventDefault();
+    
+            let element = document.querySelector("#ID");
+    
+            fetch("http://localhost:80/id/" + element.value).then((res) => {
+                return res.json();
+            })
+            .then((processed) =>{
+    
+                let reporting = document.querySelector("#reporting");
+            
+                if(processed.error){
+                    reporting.innerHTML = processed.error;
+                }
+                else{
+                    reporting.innerHTML = processed.name;
+                }
+            });
+    }
+    render() {
+        return(
+            <div>
+            <form onSubmit={this.readId}>
+                <p>Enter Pokemon ID</p>
+                <input id="ID" type ="text" />
+                <button>Submit</button>
+            </form>
+            </div>
+        );
+    }
+}
+
+export default IdSearch;
---
+++ lab6/client/src/components/NameSearch/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class NameSearch extends React.Component {
+    readName(event) {
+
+        event.preventDefault();
+
+        let element = document.querySelector("#NAME");
+
+        fetch("/name/" + element.value).then((res) => {
+                return res.json();
+        })
+        .then((processed) => {
+
+            let reporting = document.querySelector("#reportingArea");
+
+            if(processed.error) {
+                reporting.innerHTML = processed.error;
+            } else {
+                reporting.innerHTML = processed.number;
+            }
+        });
+    }
+    render() {
+        return(
+            <div>
+            <form onSubmit={this.readName}>
+                <p>Enter Pokemon Name</p>
+                <input id="NAME" type ="text" />
+                <button>Submit</button>
+            </form>
+            </div>
+        );
+    }
+}
+
+export default NameSearch;
