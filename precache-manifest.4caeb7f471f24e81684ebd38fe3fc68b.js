ba582ea650057137283aeeddb1afc0055d6aabfc
Molly Murtha <mollymurtha@Mollys-MacBook-Pro-2.local>
2020-04-18 22:43:11 -0400

    Updates

---
+++ precache-manifest.4caeb7f471f24e81684ebd38fe3fc68b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410a53f29fb033849fd875c730a715e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2bbe8aae83c7747152c",
+    "url": "/static/css/main.b40b5f71.chunk.css"
+  },
+  {
+    "revision": "e09b56ba694114c5c84e",
+    "url": "/static/js/2.5b5aab85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b5aab85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2bbe8aae83c7747152c",
+    "url": "/static/js/main.0a25d54d.chunk.js"
+  },
+  {
+    "revision": "9cb16b117985f0abc63b",
+    "url": "/static/js/runtime-main.60d3f0ab.js"
+  },
+  {
+    "revision": "2a2a9a4678fb374ee9bff6482540dc28",
+    "url": "/static/media/main.2a2a9a46.png"
+  }
+]);
